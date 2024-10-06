8738d80263b29965ddf98af9866cb16f312bf24b
CassiaValverde <126363351+CassiaValverde@users.noreply.github.com>
2024-10-05 23:13:07 -0300

    Add files via upload

---
+++ listaDeExercicio10.py
@@ -0,0 +1,5 @@
+#cáculo da área do triângulo
+n1 = float(input("Informe o valor da base "))
+n2 = float(input("Informe o valor da altura "))
+calculo = n1*n2
+print("O cálculo da área de um retângulo é ",calculo)
